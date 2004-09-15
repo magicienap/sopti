0148daf5ad0766c301d9da426eb9fce46cd80e0e
Pierre-Marc Fournier <pierre-marc.fournier@polymtl.ca>
2004-09-15 18:27:06 +0000

    *** empty log message ***

--- constraint.hpp
+++ constraint.hpp
@@ -17,6 +17,7 @@
  */
 
 #include <set>
+#include <string>
 
 #include "studentschedule.hpp"
 #include "schoolschedule.hpp"
@@ -26,7 +27,7 @@
 class Constraint
 {
 	public:
-	Constraint() {}
+	Constraint(std::string) {}
 	
 	virtual bool operator()(StudentSchedule &) = 0;
 	
@@ -36,7 +37,7 @@ class Constraint
 class NoConflicts : public Constraint
 {
 	public:
-	NoConflicts() {}
+	NoConflicts(std::string s) : Constraint(s) {}
 	
 	bool operator()(StudentSchedule &sched)
 	{
@@ -75,7 +76,7 @@ class NoConflicts : public Constraint
 class NoEvening : public Constraint
 {
 	public:
-	NoEvening() {}
+	NoEvening(std::string s) : Constraint(s) {}
 	
 	bool operator()(StudentSchedule &sched)
 	{
@@ -109,7 +110,7 @@ class NoEvening : public Constraint
 class NoClosed : public Constraint
 {
 	public:
-	NoClosed() {}
+	NoClosed(std::string s) : Constraint(s) {}
 	
 	bool operator()(StudentSchedule &sched)
 	{
@@ -134,3 +135,45 @@ class NoClosed : public Constraint
 	
 	private:
 };
+
+class NoPeriod : public Constraint
+{
+	public:
+	NoPeriod(std::string s) : Constraint(s) { 
+		char *endptr;
+		p_period = strtol(s.c_str(), &endptr, 10);
+		if(*endptr != 0) {
+			error("Invalid parameter passed to NoPeriod (%s)", s.c_str());
+		}
+	}
+	
+	bool operator()(StudentSchedule &sched)
+	{
+		const int last_allowed=1645;
+		
+		StudentSchedule::course_list_t::const_iterator it;
+		Group::period_list_t::const_iterator it2;
+		
+		// Iterate across all courses in the schedule
+		for(it=sched.st_courses_begin(); it!=sched.st_courses_end(); it++) {
+			if((*it)->theory_group) {
+				// Iterate across all periods of this course
+				for(it2=(*it)->theory_group->periods_begin(); it2!=(*it)->theory_group->periods_end(); it2++) {
+					if((*it2)->period_no() == p_period)
+						return false;
+				}
+			}
+			if((*it)->lab_group) {
+				for(it2=(*it)->lab_group->periods_begin(); it2!=(*it)->lab_group->periods_end(); it2++) {
+					if((*it2)->period_no() == p_period)
+						return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	private:
+	long p_period;
+};
+
