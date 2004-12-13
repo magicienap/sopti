7a12f54b12862c86eeb044372934bcbcbe94ea3a
Pierre-Marc Fournier <pierre-marc.fournier@polymtl.ca>
2004-12-13 03:47:03 +0000

    cosmetic modifs for AEP version

--- web/make_form.php
+++ web/make_form.php
@@ -2,22 +2,17 @@
 <html>
 
 <head>
-  <title></title>
+  <title>G&eacute;n&eacute;rer des horaires</title>
   <link rel="stylesheet" type="text/css" href="sopti.css">
 </head>
 <body>
 
 <form method="POST" action="make.php">
 
-<h1>Optimiseur d'horaire</h1>
+<h1>G&eacute;n&eacute;rer des horaires</h1>
 
-<p class="news">
 <?php
-print file_get_contents("NEWS");
-?>
-<br>
-<br>
-<?php
+/*
 $course_file="../../sopti_run/data/courses.csv";
 
 $info = stat($course_file);
@@ -25,35 +20,38 @@ $unix_modif = $info[9];
 $string_modif = date("r", $unix_modif);
 
 print "Derni&egrave;re mise a jour du fichier de cours: ".$string_modif
+*/
 ?>
-</p>
 
 <h2>1. S&eacute;lectionner les cours</h2>
-<p>&Eacute;crire les sigles des cours d&eacute;sir&eacute;s, s&eacute;par&eacute;s par un espace. Les lettres doivent &egrave;tre en majuscules (ex: ING1040)<br>
-(<a href="listcourses.php">Liste des cours offerts</a>)
-<p><textarea name="courses" cols="50" rows="2"></textarea>
+<div class="option_block">
+	<p>&Eacute;crire les sigles des cours d&eacute;sir&eacute;s, s&eacute;par&eacute;s par un espace. Les lettres doivent &egrave;tre en majuscules (ex: ING1040)<br>
+	(<a href="listcourses.php">Liste des cours offerts</a>)
+	<p><textarea name="courses" cols="50" rows="2"></textarea>
+</div>
 
-<br>
 
 <h2>2. Type d'optimisation</h2>
 <form>
-<p><input name="order" type="radio" value="minholes" checked> Minimiser les trous
-<p><input name="order" type="radio" value="maxmorningsleep"> Maximiser les heures de sommeil le matin
-<!--
-<p><input name="order" type="radio" value="maxmorning"> Concentrer le matin (pas encore impl&eacute;ment&eacute;)
-<p><input name="order" type="radio" value="maxevening"> Concentrer le soir (pas encore impl&eacute;ment&eacute;)
--->
+<div class="option_block">
+	<p><input name="order" type="radio" value="minholes" checked> Minimiser les trous
+	<p><input name="order" type="radio" value="maxmorningsleep"> Maximiser les heures de sommeil le matin
+</div>
 
-<br>
 
 <h2>3. Contraintes particuli&egrave;res</h2>
-<p><input name="noevening" type="checkbox"> Pas de cours le soir
-<p><input name="noclosed" type="checkbox" checked> Groupes avec places disponibles seulement
-<p>Nombre maximum de p&eacute;riodes avec conflit: <input type="text" name="maxconflicts" value="0" size="2">
+
+<div class="option_block">
+	<p><input name="noevening" type="checkbox"> Pas de cours le soir
+	<p><input name="noclosed" type="checkbox" checked> Groupes avec places disponibles seulement
+	<p>Nombre maximum de p&eacute;riodes avec conflit: <input type="text" name="maxconflicts" value="0" size="2">
+</div>
+
 
 <h2>4. P&eacute;riodes libres</h2>
-<p>Cochez les p&eacute;riodes qui doivent absolument &ecirc;tre libres.
-<table class="free_period_selector">
+<div class="option_block">
+	<p>Cochez les p&eacute;riodes qui doivent absolument &ecirc;tre libres.
+	<table class="free_period_selector">
 <?php
 
 $week_hours = array(830, 930, 1030, 1130, 1245, 1345, 1445, 1545, 1645);
@@ -82,11 +80,13 @@ for($i=0; $i<count($week_hours); $i++) {
 }
 
 ?>
-</table>
+	</table>
+</div>
 
 <h2>5. Terminer</h2>
-
-<p><input type="submit" value="Afficher les horaires">
+<div class="option_block">
+	<p><input type="submit" value="Afficher les horaires">
+</div>
 
 </form>
 
