221435d2e3a32c4c597a783af90ea6489989d24c
Pierre-Marc Fournier <pierre-marc.fournier@polymtl.ca>
2005-07-20 20:32:10 +0000

    *** empty log message ***

--- make_teachers_csv.sh
+++ make_teachers_csv.sh
@@ -6,7 +6,9 @@ then
 	exit 1;
 fi
 
+rm -f data/teachers.csv
+
 for file in `ls --color=never data/courses_xml/*.xml`;
 do
-	./parser_prof $file > data/teachers.csv;
+	./parser_prof $file >> data/teachers.csv;
 done
