670d82d6ad82f205839af15605e39b3730ed7363
Pierre-Marc Fournier <pierre-marc.fournier@polymtl.ca>
2005-08-07 22:17:00 +0000

    add php session support, paginate results cleanups

--- web/make.php
+++ web/make.php
@@ -1,4 +1,9 @@
 <?php
+session_start();
+if(!isset($_GET['page'])) {
+	$_SESSION = array();
+}
+
 require_once('config.php');
 require_once('lib.php');
 
@@ -58,114 +63,146 @@ require_once('config.php');
 		fwrite($counter, date("Y/m/d-H:i:s") . " " . $_SERVER['REMOTE_ADDR'] . "\n");
 		fclose($counter);
 	}
-?>
 
-<?php
-	// Prepare explicitopen arg
-	$openclose_vars_str = $_POST['openclose_vars'];
-	trim($openclose_vars_str);
-	$openclose_vars_str = trim($openclose_vars_str);
-	$oc_vars = explode(" ", $openclose_vars_str);
+	if(isset($_POST['courses'])) {
 	
-	$explicitopen_arg="";
-	foreach($oc_vars as $var) {
-		if($_POST[$var] != "") {
-			$explicitopen_arg .= $var . " ";
-		}
-	}
+		// Prepare explicitopen arg
+		$openclose_vars_str = $_POST['openclose_vars'];
+		trim($openclose_vars_str);
+		$openclose_vars_str = trim($openclose_vars_str);
+		$oc_vars = explode(" ", $openclose_vars_str);
 	
-	trim($explicitopen_arg);
-	//print($explicitopen_arg . "<br>\n");
-?>
-
-<?php
-
-	$cmd_initial=$SOPTI_EXEC . " --html make";
-	$allowed_objectives = array( "minholes", "maxmorningsleep", "maxfreedays" );
-
-	/*
-	function error($msg)
-	{
-		print("<div class=\"errormsg\">\n<p>" . $msg . "\n</div>\n");
-	}*/
-
-	// Parse
-	$courses_raw = strtoupper($_POST['courses']);
-	// Replace bizarre chars with spaces
-	$courses_raw = ereg_replace("[^A-Z0-9\.\-]", " ", $courses_raw);
-	// Replace multiple spaces with one space
-	$courses_raw = ereg_replace(" +", " ", $courses_raw);
-	// Remove initial and final whitespace
-	$courses_raw = trim($courses_raw);
+		$explicitopen_arg="";
+		foreach($oc_vars as $var) {
+			if($_POST[$var] != "") {
+				$explicitopen_arg .= $var . " ";
+			}
+		}
 	
-	if($courses_raw != "") {
-		// Split it
-		$courses = explode(" ", $courses_raw);
-		
-		
-		// Build the command string
-		$cmd=$cmd_initial;
-		foreach ($courses as $key => $val) {
-			$cmd .= " -c ".escapeshellarg($val);
-		}
-		
-		$result = array_search($_POST['order'], $allowed_objectives);
-		if($result===FALSE) {
-			error("Invalid objective");
-		}
-		$cmd .= " -J ${allowed_objectives[$result]}";
-		
-		if(strlen($explicitopen_arg)) {
-			$cmd .= " -g " . escapeshellarg($explicitopen_arg) . " -G explicitopen";
-		}
-		else {
-			$cmd .= " -g '' -G explicitopen";
-		}
-		
-		if($_POST['maxconflicts'] != "") {
-			$cmd .= " --max-conflicts ".escapeshellarg($_POST['maxconflicts']);
-		}
-		
+		trim($explicitopen_arg);
+		//print($explicitopen_arg . "<br>\n");
+
+
+		$cmd_initial=$SOPTI_EXEC . " --html make";
+		$allowed_objectives = array( "minholes", "maxmorningsleep", "maxfreedays" );
+
 		// Parse
-		$week_hours = array(830, 930, 1030, 1130, 1245, 1345, 1445, 1545, 1645); 
-		for($i=0; $i<5; $i++) {
-			for($j=0; $j<count($week_hours); $j++) {
-				$period = ($i+1)*10000+$week_hours[$j];
-				if($_POST["period_$period"] == "on") {
-					$cmd .= " -g $period -G noperiod";
+		$courses_raw = strtoupper($_POST['courses']);
+		// Replace bizarre chars with spaces
+		$courses_raw = ereg_replace("[^A-Z0-9\.\-]", " ", $courses_raw);
+		// Replace multiple spaces with one space
+		$courses_raw = ereg_replace(" +", " ", $courses_raw);
+		// Remove initial and final whitespace
+		$courses_raw = trim($courses_raw);
+	
+		if($courses_raw != "") {
+			// Split it
+			$courses = explode(" ", $courses_raw);
+			
+		
+			// Build the command string
+			$cmd=$cmd_initial;
+			foreach ($courses as $key => $val) {
+				$cmd .= " -c ".escapeshellarg($val);
+			}
+		
+			$result = array_search($_POST['order'], $allowed_objectives);
+			if($result===FALSE) {
+				error("Invalid objective");
+			}
+			$cmd .= " -J ${allowed_objectives[$result]}";
+		
+			if(strlen($explicitopen_arg)) {
+				$cmd .= " -g " . escapeshellarg($explicitopen_arg) . " -G explicitopen";
+			}
+			else {
+				$cmd .= " -g '' -G explicitopen";
+			}
+		
+			if($_POST['maxconflicts'] != "") {
+				$cmd .= " --max-conflicts ".escapeshellarg($_POST['maxconflicts']);
+			}
+		
+			// Parse
+			$week_hours = array(830, 930, 1030, 1130, 1245, 1345, 1445, 1545, 1645); 
+			for($i=0; $i<5; $i++) {
+				for($j=0; $j<count($week_hours); $j++) {
+					$period = ($i+1)*10000+$week_hours[$j];
+					if($_POST["period_$period"] == "on") {
+						$cmd .= " -g $period -G noperiod";
+					}
+				}
+			
+				if($_POST["noevening_$i"] == "on") {
+					$cmd .= " -g \"" . ($i+1) . "1745 " . ($i+1) . "2359\" -G notbetween";
 				}
 			}
+		
+			//print $cmd;
+			$handle = popen($cmd.' 2>&1', 'r');
+			$xml_groups = '';
+			while (!feof($handle)) {
+				$xml_groups .= fread($handle, 8192);
 			
-			if($_POST["noevening_$i"] == "on") {
-				$cmd .= " -g \"" . ($i+1) . "1745 " . ($i+1) . "2359\" -G notbetween";
 			}
+			pclose($handle);
+			$_SESSION['xml_groups']=$xml_groups;
 		}
-		
-		print $cmd;
-		//passthru($cmd." 2>&1");
-		$handle = popen($cmd.' 2>&1', 'r');
-		$xml_groups = '';
-		while (!feof($handle)) {
-			$xml_groups .= fread($handle, 8192);
-			
-		}
-		pclose($handle);
-		$xml = simplexml_load_string($xml_groups);
-		
+	}
+	else {
+		//echo "We are in GET (first=".$_GET['first'].") (".session_name().")<br>";
+	}
+		$show = 10;
+		$xml = simplexml_load_string($_SESSION['xml_groups']);
 		echo "<h5>Engine: Version ".$xml['engine_version']." - Compute:".$xml['compute_time']." - DB Time: ".$xml['db_time']."</h5>";
 		
-		$schedno=1;
+		$schedule_array=array();
 		foreach($xml->schedule as $sch) {
-			print_schedule($sch, $schedno);
-			$schedno++;
+			array_push($schedule_array, $sch);
+		}
+		echo "We have ".count($schedule_array)." schedules<br>\n";
+		$full_result_count = count($schedule_array);
+		$current_page = $_GET['page'];
+		//$first = $_GET['first'];
+		//$first = $first-(($first-1)%$show);
+		$first = $current_page*$show+1;
+		$page_count = (int)($full_result_count/$show);
+		if($full_result_count % $show) {
+			$page_count++;
+		}
+
+		$schedno=0;
+		for($i=$first-1; $i<$first-1+$show; $i++) {
+			if($i >= $full_result_count) {
+				break;
+			}
+			print_schedule($schedule_array[$i], $i+1);
+		}
+
+		if($page_count > 1) {
+			echo "<div class=\"page_browser\">";
+			if($current_page-1 >= 0) {
+				echo "<a href=\"make.php?page=".($current_page-1)."\">&lt;&lt;</a> ";
+			}
+			for($i=0; $i < $page_count; $i++) {
+				if($i == $current_page) {
+					echo ($i+1)." ";
+				}
+				else {
+					echo "<a href=\"make.php?page=$i\">".($i+1)."</a> ";
+				}
+			}
+			if($current_page+1 < $page_count) {
+				echo "<a href=\"make.php?page=".($current_page+1)."\">&gt;&gt;</a>";
+			}
+			echo "</div>";
 		}
 
 		$user_time=microtime(TRUE)-$user_time;
-		echo "<h5>Temps: ".$user_time."</h5>\n";
-	}
-	else {
-		error("Aucun cours spécifié. Utiliser le bouton Précédent de votre navigateur pour changer les options.");
-	}
+		echo "<h5 style=\"clear: left;\">Temps: ".$user_time."</h5>\n";
+	//else {
+		//error("Aucun cours spécifié. Utiliser le bouton Précédent de votre navigateur pour changer les options.");
+	//}
 ?>
 
 </body>
