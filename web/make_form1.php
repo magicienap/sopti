555365622069b69f9db74598906de2e41206b897
Pierre-Marc Fournier <pierre-marc.fournier@polymtl.ca>
2006-08-12 18:55:04 +0000

    convert to XHTML 1.0 strict

--- web/make_form1.php
+++ web/make_form1.php
@@ -1,30 +1,30 @@
 <?php
 # ob_start();
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/2002/REC-xhtml1-20020801/DTD/xhtml1-strict.dtd">
 <html>
 
 <head>
 	<title>Générer des horaires - Étape 1</title>
-	<link rel="stylesheet" type="text/css" href="sopti.css">
-	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+	<link rel="stylesheet" type="text/css" href="sopti.css" />
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 </head>
 
 <body>
 
 <div id="header">
 
-<img src="genhor_sm.png" alt="Générateur d'horaires">
+<img src="genhor_sm.png" alt="Générateur d'horaires" />
 
 <h1>Générer des horaires</h1>
 
-<p class="step_current">Étape 1 - Spécifier les cours désirés
-<p class="step_notcurrent">Étape 2 - Choisir les options de génération
-<p class="step_notcurrent">Étape 3 - Visualiser les horaires
+<p class="step_current">Étape 1 - Spécifier les cours désirés</p>
+<p class="step_notcurrent">Étape 2 - Choisir les options de génération</p>
+<p class="step_notcurrent">Étape 3 - Visualiser les horaires</p>
 
 </div>
 
-<form method="GET" action="make_form2.php">
+<form method="get" action="make_form2.php">
 
 <?php
 /*
@@ -40,14 +40,14 @@ print "Dernière mise a jour du fichier de cours: ".$string_modif
 
 <div class="option_block">
 	<h2>Cours désirés</h2>
-	<p>Écrire les sigles des cours désirés, séparés par un espace. Ex: MTH1101 MTH1006 INF1005C<br>
-	<a href="listcourses.php" target="_blank">Voir la liste des cours offerts</a>
-	<p><textarea name="courses" cols="50" rows="2"></textarea>
+	<p>Écrire les sigles des cours désirés, séparés par un espace. Ex: MTH1101 MTH1006 INF1005C<br />
+	<a href="listcourses.php">Voir la liste des cours offerts</a></p>
+	<p><textarea name="courses" cols="50" rows="2"></textarea></p>
 </div>
 
 
 <div class="option_block">
-	<p><input type="submit" value="Continuer...">
+	<p><input type="submit" value="Continuer..." /></p>
 </div>
 
 </form>
